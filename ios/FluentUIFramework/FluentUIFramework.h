9114f12a69be00a2d1fb6afb5842cdb7f5d7acff
Timothy Miller <innerlogic4321@gmail.com>
2023-03-20 20:54:34 +0900

    Add Framework

---
+++ ios/FluentUIFramework/FluentUIFramework.h
@@ -0,0 +1,19 @@
+//
+//  FluentUIFramework.h
+//  FluentUIFramework
+//
+//  Created by ミラー・ティモシー on 2023/03/20.
+//  Copyright © 2023 Microsoft Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FluentUIFramework.
+FOUNDATION_EXPORT double FluentUIFrameworkVersionNumber;
+
+//! Project version string for FluentUIFramework.
+FOUNDATION_EXPORT const unsigned char FluentUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluentUIFramework/PublicHeader.h>
+
+
